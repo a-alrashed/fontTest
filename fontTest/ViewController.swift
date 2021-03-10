e5aae858de2a95c203c2fc0736be88032e81c100
Azzam R Alrashed <a.alrashed@safcsp.org.sa>
2021-03-10 16:14:36 +0300

    first commit

--- fontTest/ViewController.swift
+++ fontTest/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var mainTextView: UITextView!
+    @IBOutlet weak var messageLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func send(_ sender: Any) {
+        messageLabel.text = mainTextView.text
+    }
+    
 }
 
